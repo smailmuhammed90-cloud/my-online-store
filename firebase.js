18ffbf2646949f3c5646af3870fd799090868af9
smailmuhammed90-cloud <smailmuhammed90@gmail.com>
2026-03-10 17:37:34 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,23 @@
 
+var firebaseConfig = {
+  apiKey: "AIzaSyBjIlddTv_tWkzDAKKlK_1TuK8TTYHbGJ0",
+  authDomain: "soul-store-a1075.firebaseapp.com",
+  projectId: "soul-store-a1075",
+  storageBucket: "soul-store-a1075.appspot.com",
+  messagingSenderId: "849267908668",
+  appId: "1:849267908668:web:6c85f9fcfe7b8580e72339"
+};
+
+if (typeof firebase !== 'undefined') {
+  if (!firebase.apps || !firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+  window.auth = firebase.auth();
+  if (auth && auth.setPersistence) {
+    auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL).catch(function(){});
+  }
+} else {
+  console.warn("Firebase SDK not loaded");
+}
+
+window.ADMIN_EMAILS = ["ggbrochilguy@gmail.com"];
